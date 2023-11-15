9c17ae090ccf4f06c8b3c3629b037ac984ebbdb1
Nikhil Prasad <nprasad2077@gmail.com>
2023-11-15 06:00:03 -0600

    add dotenv

--- nba_graphql/graphql_api/schema.py
+++ nba_graphql/graphql_api/schema.py
@@ -1,3 +1,35 @@
 import graphene
+from graphene_django import DjangoObjectType
+from .models import PlayerData
 
-print(graphene.__version__)
+
+class PlayerDataType(DjangoObjectType):
+    class Meta:
+        model = PlayerData
+        fields = "__all__"
+
+
+class Query(graphene.ObjectType):
+    all_players = graphene.List(
+        PlayerDataType, first=graphene.Int(), skip=graphene.Int()
+    )
+    player = graphene.Field(PlayerDataType, name=graphene.String(required=True))
+
+    def resolve_all_players(root, info, first=None, skip=None):
+        players = PlayerData.objects.all()
+        if first:
+            players = players[:first]
+        if skip:
+            players = players[skip:]
+        return players
+
+    def resolve_player(root, info, name):
+        try:
+            return PlayerData.objects.get(name=name)
+        except PlayerData.DoesNotExist:
+            return None
+
+
+schema = graphene.Schema(query=Query)
+
+
