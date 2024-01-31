fcf54b14cc1aecc791a5c5f97366060a5a04f60b
Nikhil Prasad <nprasad2077@gmail.com>
2024-01-31 08:00:20 -0600

    second query add

--- nba_graphql/graphql_api/schema.py
+++ nba_graphql/graphql_api/schema.py
@@ -7,9 +7,23 @@ class PlayerType(DjangoObjectType):
         model = PlayerData
         
 class Query(graphene.ObjectType):
+    # Search for all players in a season. All paramaters available for viewing, just specify season.
     players_by_season = graphene.List(PlayerType, season=graphene.Int(required=True))
+    
+    # Search for player by Name. You may further refine your results with optional season and team parameters after specifying player name.
+    player_by_name = graphene.List(PlayerType, name=graphene.String(required=True), season=graphene.Int(), team=graphene.String())
+
     
     def resolve_players_by_season(self, info, season, **kwargs):
         return PlayerData.objects.filter(season=season)
     
-    
+    def resolve_player_by_name(self, info, name, season=None, team=None, **kwargs):
+        search = PlayerData.objects.filter(player_name=name)
+        
+        if season:
+            search = search.filter(season=season)
+            
+        if team:
+            search = search.filter(team=team)
+        
+        return search
