b0eb8983167f719b632b6282368b05cd414184e9
Nikhil Prasad <nprasad2077@gmail.com>
2024-02-02 22:51:24 -0600

    totals db save success

--- nba_graphql/import_data_totals_playerdata.py
+++ nba_graphql/import_data_totals_playerdata.py
@@ -4,6 +4,11 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nba_graphql.settings")
+django.setup()
+
+from graphql_api.models import PlayerDataTotals
+
 # Get the website content
 season_input = input('What season? ')
 try:
@@ -28,7 +33,7 @@ table = div.find("table", attrs={"id": "totals_stats"})
 headers = table.find_all("th")
 header_values = [th.text for th in headers][:30]
 header_values.append("Player-additional")
-header_values.append("season")
+# header_values.append("season")
 
 
 # Testing output
@@ -54,16 +59,68 @@ for row in rows:
     if player_id_col:
         player_id = player_id_col["data-append-csv"]
         cols.append(player_id)
-        cols.append(season_int)
+        # cols.append(season_int)
         data_rows.append(cols)
 
 
 # Write to CSV
-
 with open(f"../data/totals/{season_input}_player_totals.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(header_values) # write headers
     writer.writerows(data_rows)
 
+print('Write to CSV success')
 
-print('success')
+
+# Write to Database
+
+def run():
+    with open(f"../data/totals/{season_input}_player_totals.csv", 'r') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            player =  PlayerDataTotals()
+            player.player_name = row['Player']
+            player.position = row['Pos']
+            
+            # Data with check
+            player.age = row['Age'] if row['Age'] else None
+            player.games = row['G'] if row['G'] else None
+            player.games_started = row['GS'] if row['GS'] else None
+            player.minutes_pg = row['MP'] if row['MP'] else None
+            player.field_goals = row['FG'] if row['FG'] else None
+            player.field_attempts = row['FGA'] if row['FGA'] else None
+            player.field_percent = row['FG%'] if row ['FG%'] else None
+            player.three_fg = row['3P'] if row['3P'] else None
+            player.three_attempts = row['3PA'] if row['3PA'] else None
+            player.three_percent = row['3P%'] if row['3P%'] else None
+            player.two_fg = row['2P'] if row['2P'] else None
+            player.two_attempts = row['2PA'] if row['2PA'] else None
+            player.two_percent = row['2P%'] if row['2P%'] else None
+            player.effect_fg_percent = row['eFG%'] if row['eFG%'] else None
+            player.ft = row['FT'] if row['FT'] else None
+            player.ft_attempts = row['FTA'] if row['FTA'] else None
+            player.ft_percent = row['FT%'] if row['FT%'] else None
+            player.offensive_rb =  row['ORB'] if row['ORB'] else None
+            player.defensive_rb = row['DRB'] if row['DRB'] else None
+            player.total_rb = row['TRB'] if row['TRB'] else None
+            player.assists = row['AST'] if row['AST'] else None
+            player.steals = row['STL'] if row['STL'] else None
+            player.blocks = row['BLK'] if row['BLK'] else None
+            player.turnovers = row['TOV'] if row['TOV'] else None
+            player.personal_fouls = row['PF'] if row['PF'] else None
+            player.points = row['PTS'] if row['PTS'] else None
+            player.team = row['Tm'] if row['Tm'] else None
+            
+            player.season = season_int
+            player.player_id = row['Player-additional']
+            
+            try:
+                player.save()
+            except Exception as e:
+                print(f'Row data: {row}')
+                print(f'Could not create record: {e}')
+
+run()
+
+print('Save to DB Success!')
