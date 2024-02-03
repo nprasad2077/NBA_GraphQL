dc4c006bc7cfd82f8e5924d30a16a5dd486ca234
Nikhil Prasad <nprasad2077@gmail.com>
2024-02-02 23:54:58 -0600

    player data advanced script create

---
+++ nba_graphql/import_data_player_advanced.py
@@ -0,0 +1,9 @@
+import os
+import django
+import requests
+from bs4 import BeautifulSoup
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nba_graphql.settings")
+django.setup()
+
+from graphql_api.models import PlayerDataAdvanced
