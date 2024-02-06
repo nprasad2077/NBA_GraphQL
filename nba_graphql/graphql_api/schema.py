75c1c9172683c191180682a9fcb3a6f20a3939a4
Nikhil Prasad <nprasad2077@gmail.com>
2024-02-06 07:07:08 -0600

    resolve by season

--- nba_graphql/graphql_api/schema.py
+++ nba_graphql/graphql_api/schema.py
@@ -22,7 +22,7 @@ class TeamDataType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     # Search for all players in a season. All paramaters available for viewing, just specify season.
-    players_by_season = graphene.List(
+    per_game_by_season = graphene.List(
         PlayerType,
         season=graphene.Int(required=True),
         team=graphene.String(),
@@ -30,19 +30,9 @@ class Query(graphene.ObjectType):
         first=graphene.Int(),
         limit=graphene.Int(),
     )
-
-    # Search for player by Name. You may further refine your results with optional season and team parameters after specifying player name.
-    player_by_name = graphene.List(
-        PlayerType,
-        name=graphene.String(required=True),
-        season=graphene.Int(),
-        team=graphene.String(),
-        ordering=graphene.String(),
-        limit=graphene.Int(),
-    )
-
+    
     # Search for team roster by abbreviation. Filter additionally by season.
-    players_by_team = graphene.List(
+    per_game_by_team = graphene.List(
         PlayerType,
         team=graphene.String(required=True),
         season=graphene.Int(),
@@ -51,10 +41,10 @@ class Query(graphene.ObjectType):
     )
     
     # Search for player totals data
-    player_totals_by_name = graphene.List(
+    totals_by_season = graphene.List(
         PlayerTotalsType,
-        name=graphene.String(required=True),
-        season=graphene.Int(),
+        name=graphene.String(),
+        season=graphene.Int(required=True),
         team=graphene.String(),
         ordering=graphene.String(),
         first=graphene.Int(),
@@ -62,10 +52,10 @@ class Query(graphene.ObjectType):
     )
     
     # Search for player advanced data
-    player_advanced_by_name = graphene.List(
+    advanced_by_season = graphene.List(
         PlayerAdvancedType,
-        name=graphene.String(required=True),
-        season=graphene.Int(),
+        name=graphene.String(),
+        season=graphene.Int(required=True),
         team=graphene.String(),
         ordering=graphene.String(),
         first=graphene.Int(),
@@ -138,7 +128,7 @@ class Query(graphene.ObjectType):
         id=graphene.Int(),
     )
 
-    def resolve_players_by_season(
+    def resolve_per_game_by_season(
         self, info, season, team=None, ordering=None, limit=None, first=None, **kwargs
     ):
         q = PlayerData.objects.filter(season=season)
@@ -157,24 +147,7 @@ class Query(graphene.ObjectType):
 
         return q
 
-    def resolve_player_by_name(self, info, name, season=None, team=None, ordering=None, limit=None, **kwargs):
-        search = PlayerData.objects.filter(player_name=name)
-
-        if season:
-            search = search.filter(season=season)
-
-        if team:
-            search = search.filter(team=team)
-        
-        if ordering:
-            search = search.order_by(ordering)
-            
-        if limit:
-            search = search[:limit]
-
-        return search
-
-    def resolve_players_by_team(self, info, team, season=None, position=None, ordering=None, **kwargs):
+    def resolve_per_game_by_team(self, info, team, season=None, position=None, ordering=None, **kwargs):
         p = PlayerData.objects.filter(team=team)
 
         if season:
@@ -188,11 +161,11 @@ class Query(graphene.ObjectType):
 
         return p
 
-    def resolve_player_totals_by_name(self, info, name, season=None, team=None, ordering=None, first=None, limit=None, **kwargs):
-        t = PlayerDataTotals.objects.filter(player_name=name)
+    def resolve_totals_by_season(self, info, name=None, season=None, team=None, ordering=None, first=None, limit=None, **kwargs):
+        t = PlayerDataTotals.objects.filter(season=season)
         
-        if season:
-            t = t.filter(season=season)
+        if name:
+            t = t.filter(player_name=name)
         
         if team:
             t = t.filter(team=team)
@@ -208,11 +181,11 @@ class Query(graphene.ObjectType):
             
         return t
     
-    def resolve_player_advanced_by_name(self, info, name, season=None, team=None, ordering=None, first=None, limit=None, **kwargs):
-        adv = PlayerDataAdvanced.objects.filter(player_name=name)
+    def resolve_advanced_by_season(self, info, name=None, season=None, team=None, ordering=None, first=None, limit=None, **kwargs):
+        adv = PlayerDataAdvanced.objects.filter(season=season)
         
-        if season:
-            adv = adv.filter(season=season)
+        if name:
+            adv = adv.filter(player_name=name)
         
         if team:
             adv = adv.filter(team=team)
