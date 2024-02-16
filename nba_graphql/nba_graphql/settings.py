d8b011fe72c74b75b6360cfee7c69e9e2af5038e
Nikhil Prasad <nprasad2077@gmail.com>
2024-02-15 18:12:35 -0600

    transfer time

--- nba_graphql/nba_graphql/settings.py
+++ nba_graphql/nba_graphql/settings.py
@@ -133,6 +133,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'nba_graphql/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
