42f90bc27e44e597fb1dc4a82cca3eec8e98a92c
Nikhil Prasad <nprasad2077@gmail.com>
2024-02-15 21:26:21 -0600

    launched

--- nba_graphql/nba_graphql/settings.py
+++ nba_graphql/nba_graphql/settings.py
@@ -131,12 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'nba_graphql/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
