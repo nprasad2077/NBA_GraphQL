c2b747df0ad53d02fbe0e903afb8c65afc26c5f7
Doctasmooth <ravi@debian-s-1vcpu-512mb-10gb-sfo3-01>
2024-02-16 05:23:17 +0000

    dropped

--- nba_graphql/nba_graphql/settings.py
+++ nba_graphql/nba_graphql/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4o=%a)51%r@6#1q0+6emoo3#rnix81s0fwd2_ndg39oed+f%b0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,12 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'nba_graphql/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
