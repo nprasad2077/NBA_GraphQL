f0cd3f5e76229cfabe7f7143f76002d45431fc21
Nikhil Prasad <nprasad2077@gmail.com>
2024-03-20 01:29:45 -0500

    team data write to db success

--- nba_graphql/import_team_data.py
+++ nba_graphql/import_team_data.py
@@ -4,6 +4,7 @@ import django
 import requests
 import re
 from bs4 import BeautifulSoup
+import datetime
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nba_graphql.settings")
 django.setup()
@@ -43,7 +44,7 @@ rows = table.find_all("tr")
 
 data_rows = []
 
-for row in rows[1:]: # Skip first row
+for row in rows[2:]: # Skip first row and second
     cols = row.find_all("td")
     th = row.find('th')
     rank = th.text if th else None
@@ -77,3 +78,67 @@ with open(f"../data/team/{team_search}.csv", "w", newline="") as file:
     writer.writerows(data_rows)
 
 print('Write to CSV success')
+
+def try_parse_float(s):
+    try:
+        return float(s)
+    except ValueError:
+        return None
+
+
+def run():
+    with open(f"../data/team/{team_abr}.csv", 'r') as file:
+        reader = csv.DictReader(file)
+        
+        current_year = datetime.datetime.now().year
+        
+        for row in reader:
+            team = TeamData()
+            
+            team.team_name = row['Team']
+            team.team_abbr = team_abr
+           # Split the season string
+            season_year_part = row['Season'].split("-")[1]
+
+            if int(season_year_part) <= current_year % 100:  # If the year part is less than or equal to current year's last 2 digits
+                season_year = int("20" + season_year_part)  # prepend '20'
+            else:
+                season_year = int("19" + season_year_part)  # prepend '19'
+
+            team.season = season_year
+            team.league = row['Lg']
+            
+            team.wins = int(row['W']) if row['W'].isdigit() else None
+            team.loss = int(row['L']) if row['L'].isdigit() else None
+            
+            team.win_loss_percent = try_parse_float(row['W/L%'])
+            team.srs = try_parse_float(row['SRS'])
+            
+            team.finish = row['Finish']
+            
+            team.pace = try_parse_float(row['Pace'])
+            team.rel_pace = try_parse_float(row['Rel Pace'])
+            team.ortg = try_parse_float(row['ORtg'])
+            team.rel_ortg = try_parse_float(row['Rel ORtg'])
+            team.drtg = try_parse_float(row['DRtg'])
+            team.rel_drtg = try_parse_float(row['Rel DRtg'])
+            team.ws = try_parse_float(row['ws'])
+
+            team.playoffs = row['Playoffs']
+            team.coaches = row['Coaches']
+            team.top_ws = row['Top WS']
+            
+            team.top_ws_player = row['top_ws_player']
+            
+            
+            
+            try:
+                team.save()
+                # print(team.__dict__)
+            except Exception as e:
+                print(f"Row Data: {row}")
+                print(f'Could not create record: {e}')
+                
+run()
+
+print('Write TeamData to DB Success!')
